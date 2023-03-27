b6ef16894dc634a70968608cc2018902a3cb55ae
Thomas Bogle <thomasbogle@Thomass-MacBook-Pro.local>
2023-03-27 16:46:32 +0100

    HandleCars function

--- src/components/CarGrid.js
+++ src/components/CarGrid.js
@@ -5,16 +5,19 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/CarGrid.css'
 import { useState, useEffect } from 'react';
 import { CarDetails } from './CarDetails';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 export function CarGrid(){
   const [cars, setCars] = useState({});
   const [showCar, setShowCar] = useState(false);
   const [carDetails, setCarDetails] = useState();
+  const [filteredCars, setFilteredCars] = useState();
 
   async function fetchAllCars () {
     const res = await fetch('http://localhost:8000/cars')
     const carsData = await res.json();
     setCars(carsData);
+    // setFilteredCars(carsData);
   }
 
   async function fetchSingleCar (id) {
@@ -27,9 +30,32 @@ export function CarGrid(){
   useEffect(() => {
     fetchAllCars();
   }, [])
+
+  // const handleCars = (e) => {
+  //   setFilteredCars()
+  //   setFilteredCars(cars.filter((car) => car.make === e));
+  // }
+
+    const handleCars = (e) => {
+        setFilteredCars(cars);
+        setFilteredCars(cars.filter((car) => car.make === e));
+    };
     
     return (
       <>
+        <Dropdown onSelect={handleCars} align={"start"} className="d-inline mx-2" autoClose="outside, true">
+          <Dropdown.Toggle  id="make-Filter">
+            Select Make
+          </Dropdown.Toggle>
+  
+          <Dropdown.Menu>
+          <Dropdown.Header> Select a manufacturer</Dropdown.Header>
+            <Dropdown.Item eventKey={'Porsche'} href="#">Porsche</Dropdown.Item>
+            <Dropdown.Item eventKey={'Lamborghini'} href="#">Lamborghini</Dropdown.Item>
+            <Dropdown.Item eventKey={'Ferrari'} href="#">Ferrari</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+        
         <Row xs={1} md={2} lg={3} className="g-4 mx-4">
           {Array.from({ length: cars.length }).map((_, idx) => (
             <Col>
@@ -42,6 +68,7 @@ export function CarGrid(){
             </Col>
           ))}
         </Row>
+
         {showCar ?         
           <CarDetails carDetails={carDetails} /> 
         : null }
--- src/pages/ShowroomPage.js
+++ src/pages/ShowroomPage.js
@@ -1,11 +1,11 @@
 import { CarGrid } from "../components/CarGrid";
 import React from "react";
-import FilterForm from "../components/FilterForm";
+// import FilterForm from "../components/FilterForm";
 
 function ShowroomPage(){
 return(
     <main>
-    <div> <FilterForm/> </div>
+    {/* <div> <FilterForm/> </div> */}
     <div>
        <CarGrid/>
     </div>
