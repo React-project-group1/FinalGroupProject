2af89e4c189818ebaaa6317d2408495b1f129d62
Thomas Bogle <thomasbogle@Thomass-MacBook-Pro.local>
2023-03-28 10:20:14 +0100

    Added sort by make and sort by price

--- src/components/CarGrid.js
+++ src/components/CarGrid.js
@@ -9,15 +9,15 @@ import Dropdown from 'react-bootstrap/Dropdown';
 
 export function CarGrid(){
   const [cars, setCars] = useState({});
+  const [carsCopy, setCarsCopy] = useState({}); // All cars data
   const [showCar, setShowCar] = useState(false);
-  const [carDetails, setCarDetails] = useState();
-  const [filteredCars, setFilteredCars] = useState();
+  const [carDetails, setCarDetails] = useState(); // single car info
 
   async function fetchAllCars () {
     const res = await fetch('http://localhost:8000/cars')
     const carsData = await res.json();
     setCars(carsData);
-    // setFilteredCars(carsData);
+    setCarsCopy(carsData);
   }
 
   async function fetchSingleCar (id) {
@@ -31,15 +31,25 @@ export function CarGrid(){
     fetchAllCars();
   }, [])
 
-  // const handleCars = (e) => {
-  //   setFilteredCars()
-  //   setFilteredCars(cars.filter((car) => car.make === e));
-  // }
+  const handleCars = (e) => {
+    setCars(carsCopy); // setCars to all data in the database
+    setCars(carsCopy.filter((car) => car.make === e));
+  };
 
-    const handleCars = (e) => {
-        setFilteredCars(cars);
-        setFilteredCars(cars.filter((car) => car.make === e));
-    };
+  const handlePrice = (e) => {
+    if (e === 'High to Low') {
+      let descending = [...carsCopy].sort((a, b) => b.cost - a.cost);
+      setCars(descending);
+    }
+    if (e === 'Low to High') {
+      let ascending = [...carsCopy].sort((a, b) => a.cost - b.cost);
+      setCars(ascending);
+    }
+  }
+
+  const clearFilter = () => {
+    setCars(carsCopy);
+  }
     
     return (
       <>
@@ -49,12 +59,29 @@ export function CarGrid(){
           </Dropdown.Toggle>
   
           <Dropdown.Menu>
-          <Dropdown.Header> Select a manufacturer</Dropdown.Header>
-            <Dropdown.Item eventKey={'Porsche'} href="#">Porsche</Dropdown.Item>
-            <Dropdown.Item eventKey={'Lamborghini'} href="#">Lamborghini</Dropdown.Item>
+          {/* <Dropdown.Header> Select a manufacturer</Dropdown.Header> */}
+            <Dropdown.Item eventKey={'BMW'} href="#">BMW</Dropdown.Item>
             <Dropdown.Item eventKey={'Ferrari'} href="#">Ferrari</Dropdown.Item>
+            <Dropdown.Item eventKey={'Lamborghini'} href="#">Lamborghini</Dropdown.Item>
+            <Dropdown.Item eventKey={'Mercedes'} href="#">Mercedes</Dropdown.Item>
+            <Dropdown.Item eventKey={'Porsche'} href="#">Porsche</Dropdown.Item>
+            <Dropdown.Item eventKey={'Rolls Royce'} href="#">Rolls Royce</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
+
+        <Dropdown onSelect={handlePrice} align={"start"} className="d-inline mx-2" autoClose="outside, true">
+          <Dropdown.Toggle  id="make-Filter">
+            Sort by price
+          </Dropdown.Toggle>
+  
+          <Dropdown.Menu>
+          {/* <Dropdown.Header>Sort by price</Dropdown.Header> */}
+            <Dropdown.Item eventKey={'Low to High'} href="#">Low to High</Dropdown.Item>
+            <Dropdown.Item eventKey={'High to Low'} href="#">High to Low</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+
+        <button className='btn btn-dark' onClick={clearFilter}>View All</button>
         
         <Row xs={1} md={2} lg={3} className="g-4 mx-4">
           {Array.from({ length: cars.length }).map((_, idx) => (
